e9ea8e942bdbc1e3bdcf9b421b9218bbb8f5aecb
blet-mac <ms@bletindia.com>
2019-06-27 17:23:45 +0530

    background change colour

--- MSStepper/ViewController.swift
+++ MSStepper/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .cyan
+        view.backgroundColor = UIColor(white: 0.69, alpha: 1)
     }
 
 
